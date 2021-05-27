7dfe1d533c59039ce0cbed629d81f1ff9ab7a628
Th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2021-05-27 18:16:42 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#setup
+#install
+
+run_setup()
+{
+sleep 2 ;
+cd $HOME ;
+ls /sdcard >/dev/null 2>&1 || termux-setup-storage && echo "[%] Settings up directory" && sleep 8 && mkdir -p /sdcard/Download/yt-downloader/{video,audio} && run_install 
+}
+
+
+run_install()
+{
+clear && echo " +++ installing dependency +++" &&
+pkg install python2 python3 ffmpeg -y && pip install youtube-dl  && echo "[+] finished installation"  || echo "[*] failed to install "  && run_install
+}
+
+
+echo "[*] Updating "
+apt update && apt upgrade
+cd $HOME
+run_setup && run_install && clear && cd $HOME/yt-downloader && ./ytd.sh || echo "something went wrong please try again ×︵×"
